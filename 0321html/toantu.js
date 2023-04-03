031a55a2b1f0804896407f8a42a87edad5db9555
loannguyen90 <nguyenloan13690@gmail.com>
2023-04-03 21:42:51 +0900

    adding bai tap

---
+++ 0321html/toantu.js
@@ -0,0 +1,8 @@
+let width = parseInt(prompt("Enter the width"));
+let height = parseInt(prompt("Enter the height")) ;
+let area = width * height;
+document.write("Width:" + width);
+document.write('<br>');
+document.write("Height:" + height);
+document.write('<br>');
+document.write("The area is:" + area);
